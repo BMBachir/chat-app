802a57874dacb843cfdb508c59626f596531eb45
BMBachir <boubaidjabachir3@gmail.com>
2024-10-03 20:11:39 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const { Server } = require("socket.io");
 const app = express();
 const port = 3000;
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 // Middleware
 app.use(express.json());
